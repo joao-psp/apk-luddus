24f746c1bb8c686c8cf7609258d050ebe2f52408
João Pedro Silva Pereira <joao.psp@hotmail.com>
2018-10-20 17:52:57 -0300

    First Commit

---
+++ app/src/main/java/com/example/joao_psp/luddus/MainLuddus.kt
@@ -0,0 +1,12 @@
+package com.example.joao_psp.luddus
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainLuddus : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_luddus)
+    }
+}
---
+++ app/src/main/java/com/example/joao_psp/luddus/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.example.joao_psp.luddus
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.content.Intent
+
+
+
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val handler = Handler()
+        handler.postDelayed({ showMainLuddus() }, 3000)
+    }
+
+    private fun showMainLuddus() {
+        val intent = Intent(
+            this, MainLuddus::class.java
+        )
+        startActivity(intent)
+        finish()
+    }
+}
